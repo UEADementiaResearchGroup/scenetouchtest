3f6fc8a126fbdf946f0d474673203cc21ddf6c4f
Tom Piercy <personalnadir@gmail.com>
2016-09-21 10:22:40 +0100

    Add extra field and info to screen

--- main.lua
+++ main.lua
@@ -2,6 +2,99 @@ display.setStatusBar(display.HiddenStatusBar)
 
 require "util.csv"
 local path=system.pathForFile("data.csv",system.DocumentsDirectory)
-log=csv.create(path,{"time","x","y","touched","date"})
+log=csv.create(path,{"time","x","y","touched","date","extra"})
 local composer=require "composer"
 composer.gotoScene("scenes.setup")
+
+-- local lfs = require "lfs"
+
+-- local imgPath = system.pathForFile("img", system.ResourceDirectory)
+
+-- local imgUsed={}
+-- for file in lfs.dir(imgPath) do
+--     -- File is the current file or directory name
+--     if file:find(".png") then
+--     	imgUsed[file]=false
+--     end
+-- end
+
+-- local scenedata=require "scenedata"
+-- for i=1, #scenedata.files do
+-- 	scenedata.load(scenedata.files[i].file)
+
+-- 	while true do
+-- 		local scene=scenedata.getNextSceneLayout()
+-- 		if not scene then
+-- 			break
+-- 		end
+-- 		if imgUsed[scene.background]==nil then
+-- 			print ("Missing file "..scene.background)
+-- 		else
+-- 			imgUsed[scene.background]=true
+-- 		end
+-- 		for k,v in ipairs(scene.objects or {}) do
+-- 			if imgUsed[v.img]==nil then
+-- 				print ("Missing file "..v.img)
+-- 			else
+-- 				imgUsed[v.img]=true
+-- 			end
+-- 			if v.mask then
+-- 				if imgUsed[v.mask]==nil then
+-- 					print ("Missing file "..v.mask)
+-- 				else
+-- 					imgUsed[v.mask]=true
+-- 				end
+-- 			end
+-- 		end
+-- 	end
+-- end
+
+-- for k,v in pairs(imgUsed) do
+-- 	if v==false then
+-- 		print (k .. " is an unused image")
+-- 	end
+-- end
+
+
+-- local group=display.newGroup()
+-- Runtime:addEventListener("tap", function() 
+-- 	local layout=scenedata.getNextSceneLayout()
+-- 	repeat 
+-- 		layout=scenedata.getNextSceneLayout()
+-- 	until not layout or layout.objects
+-- 	if not layout then
+-- 		return
+-- 	end
+
+-- 	group:removeSelf()
+-- 	group=display.newGroup()
+-- 	local bg=display.newImage(group,"img/"..layout.background)
+-- 	bg:translate(display.contentCenterX, display.contentCenterY)
+-- 	-- if not bg then
+-- 	-- 	print (layout.background .. " is missing")
+-- 	-- else
+-- 	-- 	bg:removeSelf()
+-- 	-- end
+
+-- 	for k,v in ipairs(layout.objects or {}) do
+-- 		local obj=display.newImage(group,"img/" .. v.img)
+-- 		obj.scaleX=bg.contentWidth/obj.width
+-- 		obj.scaleY=bg.contentHeight/obj.height
+-- 		if not obj then
+-- 			print (v.img .. " is missing")
+-- 		else
+-- 		  obj:translate(display.contentCenterX+(v.x or 0), display.contentCenterY+(v.y or 0))
+--  		  obj:scale((v.scale or 1) * (v.mirror and -1 or 1),v.scale or 1)
+     
+-- 			-- obj:removeSelf()
+-- 		end
+-- 		if v.mask then
+-- 			local mask=graphics.newMask("img/"..v.mask)
+-- 			if not mask then
+-- 				print (v.mask .. " is missing")
+-- 			end
+-- 			obj:setMask(mask)
+-- 		end
+-- 	end
+-- end)
+
--- scenes/scene.lua
+++ scenes/scene.lua
@@ -10,6 +10,8 @@ local ipairs=ipairs
 local graphics=graphics
 local timer=timer
 local os=os
+local native=native
+local print=print
 
 local scenedata=require "scenedata"
 local emailcsv=require "util.emailcsv"
@@ -34,7 +36,8 @@ function scene:show(event)
       ["y"]=event.y,
       ["touched"]=event.target.info,
       ["time"]=event.time,
-      ["date"]=os.date()
+      ["date"]=os.date(),
+      ["extra"]=event.target.extra
     })
     event.target:setFillColor(0,0,1)
     for i=scene.view.numChildren,1, -1 do 
@@ -54,22 +57,59 @@ function scene:show(event)
 
   local bg=display.newImage(scene.view,"img/"..layout.background)
   bg:translate(display.contentCenterX, display.contentCenterY)
-  if not layout.objects then
-    bg:addEventListener("tap", function() 
+
+  local info=display.newGroup()
+  scene.view:insert(info)
+  local textY=0
+  local t=display.newText({parent=info,text="Background: ".. layout.background,font=native.systemFont,fontSize=25})
+  t.anchorX=0
+  t.anchorY=0
+  t:setTextColor(0)
+  textY=t.height
+
+  local noInteractivity=true
+  if layout.objects then
+    for k,v in ipairs(layout.objects) do
+      if not v.no_tap then
+        noInteractivity=false
+        break
+      end
+    end
+  end
+  if 
+    noInteractivity then
+    local function close()
       for i=scene.view.numChildren,1, -1 do 
         scene.view[i]:removeSelf()
       end
       
       composer.removeScene("scenes.scene")
-      composer.gotoScene("scenes.reload",{params={scene="scenes.scene"}})  
-    end)
+      composer.gotoScene("scenes.reload",{params={scene="scenes.scene"}})    
+    end
+
+    if layout.time then
+      timer.performWithDelay(layout.time,close)
+    else
+      bg:addEventListener("tap", close)
+    end
   end
 
   for k,v in ipairs(layout.objects or {}) do
-    local obj=display.newImageRect(scene.view, "img/" .. v.img,bg.contentWidth,bg.contentHeight)
+    local obj=display.newImage(scene.view, "img/" .. v.img)
+    obj.scaleX=bg.contentWidth/obj.width
+    obj.scaleY=bg.contentHeight/obj.height
+    
     obj:translate(display.contentCenterX+(v.x or 0), display.contentCenterY+(v.y or 0))
     obj:scale((v.scale or 1) * (v.mirror and -1 or 1),v.scale or 1)
     obj.info=v.name
+    obj.extra=v.extra
+    local t=display.newText({parent=info,text="Object: ".. v.img,font=native.systemFont,fontSize=25})
+    t.anchorX=0
+    t.anchorY=0
+    t.y=textY
+    t:setTextColor(0)
+    textY=textY+t.height
+    
     if not v.no_tap then
       obj:addEventListener("tap", tap)
     end
@@ -79,6 +119,11 @@ function scene:show(event)
       obj.isHitTestMasked=true
     end
   end
+  info.anchorChildren=true
+  info.anchorX=0
+  info.anchorY=1
+  info:toFront()
+  info:translate(bg.x-bg.contentWidth/2+20, bg.y+bg.contentHeight/2-20)
   self.alpha=0
   transition.to(self.view,{alpha=1})
 end
